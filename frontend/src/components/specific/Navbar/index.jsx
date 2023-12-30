ce4a9a0afdfac6efef3aa946cbc80103e20e7cd5
m0owo <8breadsticks8@gmail.com>
2023-12-31 01:46:57 +0700

    created NavBar component

---
+++ frontend/src/components/specific/Navbar/index.jsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+    return (
+        <>
+            <div className="t-0 sticky flex flex-row p-5">
+                <span className="">PACE</span>
+            </div>
+        </>
+    );
+};
+
+export default Navbar;

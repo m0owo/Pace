65fb8b77e7113241e4be74fbdcbaef2dcad5f154
m0owo <8breadsticks8@gmail.com>
2023-12-31 01:46:26 +0700

    created TasksList page

---
+++ frontend/src/pages/TasksList/index.jsx
@@ -0,0 +1,15 @@
+import InputTask from "../../components/global/InputTask";
+import EditTask from "../../components/global/EditTask";
+import DeleteTask from "../../components/global/DeleteTask";
+import Tasks from "../../components/global/Tasks";
+
+const TasksList = () => {
+    return (
+        <>
+        <InputTask />
+        </>
+
+    );
+};
+
+export default TasksList;

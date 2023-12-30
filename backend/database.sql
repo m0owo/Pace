1530a66a2067f7a79d0ee70e731bf1beea4291ab
m0owo <8breadsticks8@gmail.com>
2023-12-31 03:02:05 +0700

    created new db table for tasks

--- backend/database.sql
+++ backend/database.sql
@@ -4,3 +4,13 @@ CREATE TABLE todo(
     todo_id SERIAL PRIMARY KEY,
     description VARCHAR(255)
 );
+
+CREATE TABLE task (
+    task_id SERIAL PRIMARY KEY,
+    task_description VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    status VARCHAR(50) DEFAULT 'To Do',
+    completed_at TIMESTAMP
+);
+
+

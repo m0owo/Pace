d593f183e314c4eb148afb3923fb35f32d4dd75e
m0owo <8breadsticks8@gmail.com>
2023-12-31 01:47:35 +0700

    added Navbar to App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import AnimatedRoutes from "./components/specific/AnimatedRoutes";
+import Navbar from "./components/specific/Navbar";
 
 function App() {
 	return (
 		<Router>
+			<Navbar />
 			<AnimatedRoutes />
 		</Router>
 	)

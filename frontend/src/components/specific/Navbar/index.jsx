736d170fb81700f803b3f527e2ff267122793048
m0owo <8breadsticks8@gmail.com>
2023-12-31 13:11:25 +0700

    navigation bar at the bottom

--- frontend/src/components/specific/Navbar/index.jsx
+++ frontend/src/components/specific/Navbar/index.jsx
@@ -1,11 +1,33 @@
+import { RiCalendarTodoLine } from "react-icons/ri";
+import { NavLink } from "react-router-dom";
+
 const Navbar = () => {
     return (
         <>
-            <div className="t-0 sticky flex flex-row p-5">
-                <span className="">PACE</span>
+            <div className="flex justify-center bottom-0 right-0 left-0 fixed p-2">
+                <div className="flex flex-row justify-center m-2 rounded-xl 
+                              bg-blue-200 w-full p-2">
+                    <NavbarLink link="/Pace/tasks/" />
+                </div>
             </div>
         </>
     );
 };
+
+const NavbarLink = ({ link }) => {
+    return (
+        <>
+            <NavLink className="p-2 rounded-lg bg-gray-100 hover:bg-gray-700
+                                hover:cursor-pointer group flex flex-col 
+                                justify-center" 
+                     to={link}>
+                <RiCalendarTodoLine className="m-auto text-xl group-hover:text-red-100" />
+                <p className="text-center text-sm group-hover:text-red-100">
+                    Tasks
+                </p>
+            </NavLink>
+        </>
+    )
+}
 
 export default Navbar;

69988d447a4e0f1a07d1239d6c7c1b4a3a8bf4e0
m0owo <8breadsticks8@gmail.com>
2023-12-31 15:09:04 +0700

    can change task status from to do list

--- frontend/src/components/global/Tasks/index.jsx
+++ frontend/src/components/global/Tasks/index.jsx
@@ -3,35 +3,19 @@ import { IoMdArrowRoundBack } from "react-icons/io";
 import { RiCheckboxBlankCircleLine } from "react-icons/ri"
 import { RiCheckboxCircleFill } from "react-icons/ri";
 
-// const addTask = async (e) => {
-//     e.preventDefault();
-//     try {
-//         const taskInput = { task_description: taskDescription };
-//         const res = await fetch(
-//             "http://localhost:3000/tasks",
-//             {
-//                 method: "POST",
-//                 headers: { "Content-Type": "application/json" },
-//                 body: JSON.stringify(taskInput),
-//             }
-//         );
-//         console.log(JSON.stringify(taskInput));
-//         console.log(res);
-//         setTaskDescription("");
-//     } catch (err) {
-//         console.error(err.message);
-//     }
-// };
-
 const Tasks = () => {
     const [allTasks, setAllTasks] = useState([]);
 
     async function getTasks() {
-        const res = await fetch(
-            "http://localhost:3000/tasks"
-        );
-        const allTasksArray = await res.json();
-        setAllTasks(allTasksArray);
+        try {
+            const res = await fetch(
+                "http://localhost:3000/tasks"
+            );
+            const allTasksArray = await res.json();
+            setAllTasks(allTasksArray);
+        } catch (error) {
+            console.error(error.message);
+        }
     };
 
     useEffect(() => {
@@ -46,21 +30,18 @@ const Tasks = () => {
                 <IoMdArrowRoundBack className='text-2xl my-auto
                                                rounded-full hover:bg-pink-100
                                                hover:text-white' />
-                <h1 className="text-2xl sm:text-5xl sm:mx-0 text-center 
+                <h1 className="text-4xl sm:text-5xl sm:mx-0 text-center 
                                transition-all font-bold p-5 mx-auto"
                 >
-                    My To-do List
+                    My Project
                 </h1>
             </div>
-            <div className="w-full p-10 flex flex-col gap-4 justify-center">
+            <div className="w-full p-10 flex flex-col gap-4 justify-center mb-40">
                 {
                     allTasks.map(task => (
                             <Task 
                                   key={task.task_id}
-                                  description={task.task_description}
-                                  status={task.status}
-                                  created={task.created_at}
-                                  completed={task.complete_at}
+                                  task={task}
                             />
                     ))
                 }
@@ -69,17 +50,58 @@ const Tasks = () => {
     );
 };
 
-const Task = ({description, status, created, completed}) => {
-    const [currentStatus, setCurrentStatus] = useState(status);
+const Task = ({task}) => {
+    const [currentStatus, setCurrentStatus] = useState(task.status);
+
+    async function changeStatus() {
+        try {
+            const newStatus = currentStatus === "Done" ? "To Do" : "Done";
+            const res = await fetch(
+                `http://localhost:3000/tasks/${task.task_id}/status`,
+                {
+                    method: "PUT",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify({ status: newStatus }),
+                }
+            );
+            console.log(JSON.stringify({ status: newStatus }));
+            console.log(await res.json());
+            setCurrentStatus(newStatus); // Update state after successful API call
+        } catch (error) {
+            console.error(error.message);
+        }
+    }    
     return (
         <>
             <div className='p-10 sm:p-16 w-full bg-pink-100 
-                            rounded-xl flex flex-col'>
-                <div className='flex flex-row gap-5'>
-                    {(currentStatus == "Done") ? 
-                        <RiCheckboxCircleFill className='text-3xl my-auto'/> :
-                        <RiCheckboxBlankCircleLine className='text-2xl my-auto' />}
-                    <p>{description}</p>
+                            rounded-xl flex flex-col shadow-md
+                            gap-4 relative'>
+                <div className='absolute top-5 right-5 flex flex-row justify-end'>
+                    <p className='text-xs opacity-50'>
+                        {(currentStatus == "Done") ?
+                        `Date Completed ${task.completed_at}` :
+                        `Date Added ${task.created_at}` 
+                        }
+                    </p>
+                </div>
+                <div className='h-[90%] flex flex-row gap-5'>
+                    <div onClick={changeStatus}
+                         className='flex flex-row transition-all'>
+                            <RiCheckboxCircleFill className={`my-auto text-2xl transition-all
+                                                              ${(currentStatus == "Done") ? 
+                                                                "opacity-100 scale-100" : 
+                                                                "opacity-0 scale-0 w-0"}`}
+                                                  onClick={() => setCurrentStatus("To Do")}
+                            />
+                            <RiCheckboxBlankCircleLine className={`my-auto text-2xl transition-all
+                                                                   ${(currentStatus == "Done") ? 
+                                                                   "opacity-0 scale-0 w-0": 
+                                                                   "opacity-100 scale-100"}`}
+                                                  onClick={() => setCurrentStatus("Done")}
+                            />
+
+                    </div>
+                    <p className={`${(currentStatus == "Done") ? "line-through opacity-40" : ""}`}>{task.task_description}</p>
                 </div>
             </div>
         </>

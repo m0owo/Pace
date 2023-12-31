586608b6ae92f46628774cdf2a71a01fa218ec56
m0owo <8breadsticks8@gmail.com>
2023-12-31 21:41:25 +0700

    added home page link to navbar

--- frontend/src/components/specific/Navbar/index.jsx
+++ frontend/src/components/specific/Navbar/index.jsx
@@ -1,27 +1,34 @@
 import { RiCalendarTodoLine } from "react-icons/ri";
+import { AiOutlineHome } from "react-icons/ai";
 import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
     return (
         <>
             <div className="flex justify-center bottom-0 right-0 left-0 fixed p-2 z-10">
-                <div className="flex flex-row justify-center m-2 rounded-xl 
-                              bg-gray-700 w-full p-2">
-                    <NavbarLink link="/Pace/tasks/" />
+                <div className="flex flex-row justify-center m-2 rounded-xl gap-10
+                              bg-gray-700 w-full p-2"
+                >
+                    <NavbarLink icon={<AiOutlineHome className="m-auto text-xl text-red-100 group-hover:text-black" />}
+                                link="/Pace/" />    
+                    <NavbarLink icon={<RiCalendarTodoLine className="m-auto text-xl text-red-100 group-hover:text-black" />}
+                                link="/Pace/tasks/" />
+
                 </div>
             </div>
         </>
     );
 };
 
-const NavbarLink = ({ link }) => {
+const NavbarLink = ({icon, link }) => {
     return (
         <>
             <NavLink className="p-2 rounded-lg hover:bg-gray-100 bg-gray-700
                                 hover:cursor-pointer group flex flex-col 
                                 justify-center shadow-black shadow-md" 
-                     to={link}>
-                <RiCalendarTodoLine className="m-auto text-xl text-red-100 group-hover:text-black" />
+                     to={link}
+            >
+                {icon}
                 <p className="text-center text-sm text-red-100 group-hover:text-black">
                     Tasks
                 </p>

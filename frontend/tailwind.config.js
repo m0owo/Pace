3b80dec025d6d51f502ad182b011dccc77e682df
m0owo <8breadsticks8@gmail.com>
2023-12-31 23:27:09 +0700

    imported fonts

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'akrasia': ["AkrasiaVP Demo"],
+        'dillan': ["MADE Dillan"],
+        'kento': ["Kento"],
+        'helveticaneue': ["Helvetica Neue"]
+      }
+    },
   },
   plugins: [],
 }

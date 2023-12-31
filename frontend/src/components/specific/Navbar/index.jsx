c594fbc9b71f245864117527b245efc52002e9cf
m0owo <8breadsticks8@gmail.com>
2023-12-31 23:28:14 +0700

    style

--- frontend/src/components/specific/Navbar/index.jsx
+++ frontend/src/components/specific/Navbar/index.jsx
@@ -7,12 +7,12 @@ const Navbar = () => {
         <>
             <div className="flex justify-center bottom-0 right-0 left-0 fixed p-2 z-10">
                 <div className="flex flex-row justify-center m-2 rounded-xl gap-10
-                              bg-gray-700 w-full p-2"
+                                w-full p-2 bg-[#212]"
                 >
-                    <NavbarLink icon={<AiOutlineHome className="m-auto text-xl text-red-100 group-hover:text-black" />}
-                                link="/Pace/" />    
-                    <NavbarLink icon={<RiCalendarTodoLine className="m-auto text-xl text-red-100 group-hover:text-black" />}
-                                link="/Pace/tasks/" />
+                    <NavbarLink icon={<AiOutlineHome className="m-auto text-xl text-[#cc2048] group-hover:text-[#4c0e23]" />}
+                                link="/Pace/" label="Home" />    
+                    <NavbarLink icon={<RiCalendarTodoLine className="m-auto text-xl text-[#cc2048] group-hover:text-[#4c0e23]" />}
+                                link="/Pace/tasks/" label="Tasks" />
 
                 </div>
             </div>
@@ -20,17 +20,17 @@ const Navbar = () => {
     );
 };
 
-const NavbarLink = ({icon, link }) => {
+const NavbarLink = ({icon, link, label}) => {
     return (
         <>
-            <NavLink className="p-2 rounded-lg hover:bg-gray-100 bg-gray-700
-                                hover:cursor-pointer group flex flex-col 
-                                justify-center shadow-black shadow-md" 
+            <NavLink className="p-2 rounded-lg hover:cursor-pointer group
+                                flex flex-col transition-all justify-center" 
                      to={link}
             >
                 {icon}
-                <p className="text-center text-sm text-red-100 group-hover:text-black">
-                    Tasks
+                <p className="text-center text-sm text-[#cc2048]
+                            group-hover:text-[#4c0e23] font-akrasia">
+                {label}
                 </p>
             </NavLink>
         </>

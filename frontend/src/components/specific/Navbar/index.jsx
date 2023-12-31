3c03a00223f8251b0ec2c14f05ffac89728b0f21
m0owo <8breadsticks8@gmail.com>
2023-12-31 15:09:19 +0700

    style changes

--- frontend/src/components/specific/Navbar/index.jsx
+++ frontend/src/components/specific/Navbar/index.jsx
@@ -4,9 +4,9 @@ import { NavLink } from "react-router-dom";
 const Navbar = () => {
     return (
         <>
-            <div className="flex justify-center bottom-0 right-0 left-0 fixed p-2">
+            <div className="flex justify-center bottom-0 right-0 left-0 fixed p-2 z-10">
                 <div className="flex flex-row justify-center m-2 rounded-xl 
-                              bg-blue-200 w-full p-2">
+                              bg-gray-700 w-full p-2">
                     <NavbarLink link="/Pace/tasks/" />
                 </div>
             </div>
@@ -17,12 +17,12 @@ const Navbar = () => {
 const NavbarLink = ({ link }) => {
     return (
         <>
-            <NavLink className="p-2 rounded-lg bg-gray-100 hover:bg-gray-700
+            <NavLink className="p-2 rounded-lg hover:bg-gray-100 bg-gray-700
                                 hover:cursor-pointer group flex flex-col 
-                                justify-center" 
+                                justify-center shadow-black shadow-md" 
                      to={link}>
-                <RiCalendarTodoLine className="m-auto text-xl group-hover:text-red-100" />
-                <p className="text-center text-sm group-hover:text-red-100">
+                <RiCalendarTodoLine className="m-auto text-xl text-red-100 group-hover:text-black" />
+                <p className="text-center text-sm text-red-100 group-hover:text-black">
                     Tasks
                 </p>
             </NavLink>
